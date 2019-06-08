6d7907958d7abd92c396a36d2a7a988f8bda1fea
Jas <c.jasminher@gmail.com>
2019-06-08 17:01:09 -0400

    Updates

---
+++ precache-manifest.58c3053b7c3e3700a6c4692bd088fdee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c2ace80f4d217fe5d51d295e83e6fb",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "5af4e182968cd0065c88",
+    "url": "/repository-name/static/css/main.8059ae75.chunk.css"
+  },
+  {
+    "revision": "e711f691d3cd19de11b4",
+    "url": "/repository-name/static/js/2.fcad81e9.chunk.js"
+  },
+  {
+    "revision": "5af4e182968cd0065c88",
+    "url": "/repository-name/static/js/main.3dff82bb.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  }
+]);
